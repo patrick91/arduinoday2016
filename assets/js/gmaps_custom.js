ebf246c2e1c597da1179f737da26a01ea1a01ad5
dslak <token@token.token>
2016-03-02 17:02:34 +0100

    first commit

---
+++ assets/js/gmaps_custom.js
@@ -0,0 +1,77 @@
+// Google Map Customization
+
+
+	var map;
+
+	map = new GMaps({
+		el: '#gmap',
+		lng: 14.7093593,
+		lat: 40.6981909,
+		scrollwheel:true,
+		zoom: 19,
+		zoomControl : false,
+		panControl : false,
+		streetViewControl : false,
+		mapTypeControl: false,
+		overviewMapControl: false,
+		clickable: false
+	});
+
+	var image1 = 'assets/img/marker.svg';
+	map.addMarker({
+		lng: 14.7093593,
+		lat: 40.6981909,
+		icon: image1,
+		animation: google.maps.Animation.DROP,
+		verticalAlign: 'bottom',
+		horizontalAlign: 'center',
+		backgroundColor: '#3e8bff',
+	});
+
+
+	var styles = [ 
+
+	{
+		"featureType": "road",
+		"stylers": [
+		{ "color": "#86A7AA" }
+// 		{ "color": "#F7AB24" }
+		]
+	}/*,{
+		"featureType": "water",
+		"stylers": [
+		{ "color": "#d8d8d8" }
+		]
+	},{
+		"featureType": "landscape",
+		"stylers": [
+		{ "color": "#f0f0f0" }
+		]
+	},{
+		"elementType": "labels.text.fill",
+		"stylers": [
+		{ "color": "#000000" }
+		]
+	},{
+		"featureType": "poi",
+		"stylers": [
+		{ "color": "#FF8000" }
+		]
+	},*/,{
+		"elementType": "labels.text",
+		"stylers": [
+		{ "saturation": 1 },
+		{ "weight": 0.1 },
+		{ "color": "#000000" }
+		]
+	}
+
+	];
+
+	map.addStyle({
+		styledMapName:"Styled Map",
+		styles: styles,
+		mapTypeId: "map_style"  
+	});
+
+	map.setStyle("map_style");
